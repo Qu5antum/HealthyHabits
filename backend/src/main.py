01a4ccbff322bc12021f04383240e685df4a3478
Qu5antum <erae88959@gmail.com>
2025-12-07 10:37:05 +0300

    added current user dependencies

--- backend/src/main.py
+++ backend/src/main.py
@@ -5,6 +5,7 @@ from backend.src.services.user_service import *
 from backend.src.services.habit_service import *
 from backend.src.services.reminder_service import *
 from backend.src.models.schemas import *
+from backend.src.dependencies.dependencies import get_current_user
 import asyncio
 import uvicorn
 
@@ -20,69 +21,58 @@ async def register_user(user: UserCreate, session: AsyncSession = Depends(get_se
 @app.post("/login/")
 async def login_user(credents: OAuth2PasswordRequestForm = Depends(), session: AsyncSession = Depends(get_session)):
     try:
-        user = await auth_user(session, credents)
+        user = await auth_user(credents, session)
         return user
     except Exception as e:
-        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail="User not logined")
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="User not logined")
 
-@app.post("/user/{user_id}/habit")
+@app.post("/habit/")
 async def add_habits(
-    user_id: int,
     habit: HealthyHabitCreate,
+    current_user: User = Depends(get_current_user),
     session: AsyncSession = Depends(get_session)
 ):
-    try:
-        return await add_new_habit_by_user_id(
-            session=session,
-            user_id=user_id,
-            title=habit.title,
-            description=habit.description,
-            goal=habit.goal
-        )
-    except Exception as e:
-        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Habit not added!")
+    return await add_new_habit_by_user_id(
+        session=session,
+        user_id=current_user.id,
+        title=habit.title,
+        description=habit.description,
+        goal=habit.goal
+    )
 
-@app.get("/user/{user_id}/habit")
+
+@app.get("/habit/")
 async def get_habits(
-    user_id: int,
+    user: User = Depends(get_current_user),
     session: AsyncSession = Depends(get_session)
 ):
-    try:
-        result = await get_habit_by_user_id(session, user_id)
-        return result
-    except Exception as e:
-        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Error!")
+    result = await get_habit_by_user_id(session, user.id)
+    return result
 
-@app.put("/user/{user_id}/habit")
+@app.put("/habit/")
 async def update_habit(
-    user_id: int,
     habit_id: int,
     habit: HealthyHabitCreate,
+    user: User = Depends(get_current_user),
     session: AsyncSession = Depends(get_session)
 ):
-    try:
-        return await update_habit_by_user_id(
-            session=session,
-            user_id=user_id,
-            habit_id=habit_id,
-            title=habit.title,
-            description=habit.description,
-            goal=habit.goal
-        )
-    except Exception as e:
-        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Habits not updated")
+    return await update_habit_by_user_id(
+        session=session,
+        user_id=user.id,
+        habit_id=habit_id,
+        title=habit.title,
+        description=habit.description,
+        goal=habit.goal
+    )
 
-@app.delete("/user/{user_id}/habit")
+@app.delete("/habit/")
 async def delete_habit(
-    user_id: int,
     habit_id: int,
+    user: User = Depends(get_current_user),
     session: AsyncSession = Depends(get_session)
 ):
-    try:
-        await delete_habit_by_user_id(session, user_id, habit_id)
-        return {"message": "Habit is deleted!"}
-    except Exception as e:
-        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Habits not deleted!")
+    await delete_habit_by_user_id(session, user.id, habit_id)
+    return {"message": "Habit is deleted!"}
 
 @app.post("/user/{user_id}/habit/{habit_id}/reminder")
 async def add_reminder(
@@ -91,27 +81,21 @@ async def add_reminder(
     reminder: RemindersCreate,
     session: AsyncSession = Depends(get_session)
 ):
-    try:
-        return await add_reminder_by_habit_id(
-            session=session,
-            user_id=user_id,
-            habit_id=habit_id,
-            reminder_name=reminder.name,
-            reminder_time=reminder.time
-        )               
-    except Exception as e:
-        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Reminder is not added!")
+    return await add_reminder_by_habit_id(
+        session=session,
+        user_id=user_id,
+        habit_id=habit_id,
+        reminder_name=reminder.name,
+        reminder_time=reminder.time
+    )               
 
 @app.get("/user/{user_id}/habit/{habit_id}/reminder")
 async def get_reminder(
     habit_id: int,
     session: AsyncSession = Depends(get_session)
 ):
-    try:
-        result = await get_reminder_by_habit_id(session, habit_id)
-        return result
-    except Exception as e:
-        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Error!")
+    result = await get_reminder_by_habit_id(session, habit_id)
+    return result
 
 if __name__ == "__main__":
     asyncio.run(init_models())
--- backend/src/security/security.py
+++ backend/src/security/security.py
@@ -18,7 +18,7 @@ async def create_jwt_token(data):
     encoded_jwt = jwt.encode(to_encode, config.SECRET_KEY, algorithm=config.ALGORITHM)
     return encoded_jwt
 
-async def auth_user(session: AsyncSession, credents: OAuth2PasswordRequestForm):
+async def auth_user(credents: OAuth2PasswordRequestForm, session: AsyncSession = Depends(get_session)):
     query = select(User).where(User.username == credents.username)
     result = await session.execute(query)
     user = result.scalar_one_or_none()
@@ -35,16 +35,16 @@ async def auth_user(session: AsyncSession, credents: OAuth2PasswordRequestForm):
             detail="Incorrect password"
         )
     
-    token = await create_jwt_token({"sub": user.username})
+    token = await create_jwt_token({"sub": str(user.id)})
     return {"access_token": token}
 
 async def get_user_from_token(token: str = Depends(oauth2_scheme)):
     try:
         playload = jwt.decode(token, config.SECRET_KEY, algorithms=[config.ALGORITHM])
-        username = playload.get("sub")
-        if username is None:
+        user_id: int = playload.get("sub")
+        if user_id is None:
             raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="Invalid token!")
-        return username
+        return user_id
     except jwt.ExpiredSignatureError:
         raise HTTPException(status_code=401, detail="The token has expired")
     except jwt.InvalidTokenError:
